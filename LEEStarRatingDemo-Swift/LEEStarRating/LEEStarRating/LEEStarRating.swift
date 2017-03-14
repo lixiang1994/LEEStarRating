733c4e445f372694634dc6b00e5ec91f6319d467
李响 <applelixiang@126.com>
2017-03-14 13:42:20 +0800

    完成oc版 创建swift工程

---
+++ LEEStarRatingDemo-Swift/LEEStarRating/LEEStarRating/LEEStarRating.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+class LEEStarRating: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+}
---
+++ LEEStarRatingDemo-Swift/LEEStarRating/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LEEStarRating
+//
+//  Created by 李响 on 2017/3/14.
+//  Copyright © 2017年 lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
